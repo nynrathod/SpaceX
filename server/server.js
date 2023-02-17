872968cc120997bb68939f525c84aa637500dd68
nynrathod <64688014+nynrathod@users.noreply.github.com>
2023-02-17 19:34:22 +0530

    server file updated

--- server/server.js
+++ server/server.js
@@ -1,10 +1,16 @@
 const express = require('express')
+const getConnection = require('./dbconnect');
+
 const app = express()
 const port = 3000
+var cors = require('cors')
 
-app.get('/', (req, res) => {
-	res.send('Hello World!')
-})
+getConnection();
+app.use(cors());
+app.use(express.json())
+
+// Available Routes
+app.use('/api/form', require('./routes/form'))
 
 app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`)
